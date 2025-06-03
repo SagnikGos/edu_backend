d236511fe949ae05a471c6b20a46a742b05e3904
SagnikGos <sagnik.gos06@gmail.com>
2025-06-04 00:59:16 +0530

    added GET /api/v1/chapters endpoint

--- src/controllers/chapterController.js
+++ src/controllers/chapterController.js
@@ -88,3 +88,62 @@ export const uploadChapters = async (req, res) => {
     failedChapters: failedChapters,
   });
 };
+
+
+export const getAllChapters = async (req, res) => {
+  try {
+    const {
+      subject,
+      class: className, 
+      unit,
+      status,
+      weakChapters,
+      page = 1,
+      limit = 10,
+    } = req.query;
+
+    const queryOptions = {};
+
+    if (subject) queryOptions.subject = subject;
+    if (className) queryOptions.class = className; 
+    if (unit) queryOptions.unit = unit;
+    if (status) queryOptions.status = status;
+    if (subject) queryOptions.subject = subject;
+
+    
+    if (weakChapters !== undefined) {
+      queryOptions.isWeakChapter = weakChapters === 'true';
+    }
+
+    const pageNumber = parseInt(page, 10);
+    const limitNumber = parseInt(limit, 10);
+
+    if (isNaN(pageNumber) || pageNumber < 1) {
+        return res.status(400).json({ message: "Invalid page number. Must be 1 or greater." });
+    }
+    if (isNaN(limitNumber) || limitNumber < 1) {
+        return res.status(400).json({ message: "Invalid limit number. Must be 1 or greater." });
+    }
+
+    const skip = (pageNumber - 1) * limitNumber;
+
+    
+    const totalChapters = await Chapter.countDocuments(queryOptions);
+
+    const chapters = await Chapter.find(queryOptions)
+      .sort({ createdAt: -1 }) 
+      .skip(skip)
+      .limit(limitNumber);
+
+    res.status(200).json({
+      message: 'Chapters retrieved successfully',
+      totalChapters,
+      currentPage: pageNumber,
+      totalPages: Math.ceil(totalChapters / limitNumber),
+      chapters,
+    });
+  } catch (error) {
+    console.error('Error fetching chapters:', error);
+    res.status(500).json({ message: 'Server error while fetching chapters', error: error.message });
+  }
+};
--- src/routes/chapterRoutes.js
+++ src/routes/chapterRoutes.js
@@ -2,15 +2,24 @@ import express from 'express';
 import { uploadChapters } from '../controllers/chapterController.js';
 import adminAuth from '../middlewares/adminAuth.js';
 import upload from '../middlewares/upload.js';
+import { getAllChapters } from '../controllers/chapterController.js';
+import { cacheAllChapters } from '../middlewares/cacheMiddleware.js';
 
 const router = express.Router();
 
-
+// POST /api/v1/chapters
 router.post(
   '/chapters',
   adminAuth,
   upload.single('chaptersFile'),
   uploadChapters
+);
+
+// GET /api/v1/chapters
+router.get(
+  '/chapters',
+  cacheAllChapters, 
+  getAllChapters
 );
 
 export default router;
