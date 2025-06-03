c34981a2c712b43daef42110e0a0ec9f475aac6c
SagnikGos <sagnik.gos06@gmail.com>
2025-06-04 00:06:02 +0530

    initial push

---
+++ server.js
@@ -0,0 +1,23 @@
+// server.js
+import express from 'express';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001; 
+
+app.use(express.json());
+
+// Test Route
+app.get('/', (req, res) => {
+  res.send('Chapter Performance Dashboard API is running!');
+});
+
+// Server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+export default app; 
